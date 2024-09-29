4fca3f58c2f66f2dd9659cc919e232b4327d7f1b
saihein2480 <u6520051@au.edu>
2024-09-29 14:41:21 +0700

    Initial commit

--- src/app/api/auth/login/route.js
+++ src/app/api/auth/login/route.js
@@ -1,8 +1,6 @@
-// src/app/api/auth/login/route.js
 import dbConnect from "@/app/utils/dbConnect";
 import User from "@/app/models/User";
 import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken"; // Import JWT library
 
 export async function POST(req) {
   try {
@@ -17,21 +15,11 @@ export async function POST(req) {
 
     // If user exists and password is correct
     if (user && (await bcrypt.compare(password, user.password))) {
-      // Generate a JWT token
-      const token = jwt.sign(
-        {
-          userId: user._id,
-          role: user.role,
-        },
-        process.env.JWT_SECRET // Use your secret key from environment variables
-      );
-
-      // Return success response with JWT token
+      // Return success response with user details (without sensitive data)
       return new Response(
         JSON.stringify({
           success: true,
           message: "Login successful",
-          token, // Send the JWT token to the client
           role: user.role,
           userId: user._id,
         }),
